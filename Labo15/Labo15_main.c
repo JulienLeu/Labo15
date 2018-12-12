c4a60221a1cdd0cec2191e709af882a8766aa16d
Julien <julien.leuleu@gmail.com>
2018-12-12 13:22:12 +0100

    Début

---
+++ Labo15/Labo15_main.c
@@ -0,0 +1,25 @@
+/* Fichier : Labo07.c
+ * Auteur  : Julien Leuenberger
+ * Date    : 30.10.2018
+ *
+ * Description :	Le programme permet de calculer un interet,
+ *					convertir des euros en frs et vice-versa.
+ *					Un petit menu facilite la navigation
+ * Remarque(s) :
+ *
+ * Modifications : Date / Auteur / Raison
+ *
+ * Compilateur : Visual Studio 2017
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Prototypes de fonctions*/
+int main(void)
+{
+
+
+	system("PAUSE");
+	return EXIT_SUCCESS;
+
+}
